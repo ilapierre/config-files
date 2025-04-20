b5508e5ba33f1d01b2e53ff20e8af25f72618330
Ian Lapierre <ian.lapierre@live.ca>
2025-04-20 16:03:37 -0400

    Safety commit before refactor.

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -2,7 +2,7 @@ vim.opt.wrap = false -- disable text wrapping
 
 -- line number
 vim.opt.number = true
-vim.opt.relativenumber = false
+vim.opt.relativenumber = true
 
 -- clipboard
 vim.opt.clipboard:append { 'unnamed', 'unnamedplus' }
--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -1,21 +1,24 @@
 return {
-  "stevearc/oil.nvim",
-  lazy = false,
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+	"stevearc/oil.nvim",
+	lazy = false,
+	dependencies = { "nvim-tree/nvim-web-devicons" },
 	opts = {
-		columns = {"icon"},
+		columns = { "icon" },
 		view_options = {
 			show_hidden = true
 		},
-		    preview = {
-		      float = true,
-		    },
+		preview = {
+			float = true,
+		},
+		win_options = {
+			signcolumn = "yes:2",
+		}
 	},
-	config = function (_, opts)
+	config = function(_, opts)
 		local oil = require("oil")
 		oil.setup(opts)
 
 		vim.keymap.set("n", "<leader>e", oil.toggle_float)
-		vim.keymap.set("n", "-", "<CMD>Oil<CR>", {desc = "Open parent directory"})
+		vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 	end
 }
